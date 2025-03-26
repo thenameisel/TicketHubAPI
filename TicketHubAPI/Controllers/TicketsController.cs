ed7411c91abb219e6f3d03a8329c6ce712c253dc
thenameisel <thenameiselwisman@gmail.com>
2025-03-26 09:46:14 -0300

    Set up basic get/post

---
+++ TicketHubAPI/Controllers/TicketsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketHubAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly ILogger<TicketsController> _logger;
+        private readonly IConfiguration _config;
+
+        public TicketsController(ILogger<TicketsController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from MyContactsController - Get()");
+        }
+
+        [HttpPost]
+        public IActionResult Post(Ticket ticket)
+        {
+            return Ok("Hello " + ticket.name + " from MyContactsController - Post()");
+        }
+    }
+}
---
+++ TicketHubAPI/Ticket.cs
@@ -0,0 +1,36 @@
+﻿namespace TicketHubAPI
+{
+    public class Ticket
+    {
+
+        //        {
+        //  "concertId": 0,
+        //  "email": "string",
+        //  "name": "string",
+        //  "phone": "string",
+        //  "quantity": 0,
+        //  "creditCard": "string",
+        //  "expiration": "string",
+        //  "securityCode": "string",
+        //  "address": "string",
+        //  "city": "string",
+        //  "province": "string",
+        //  "postalCode": "string",
+        //  "country": "string"
+        //}
+        public int concertId { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public int quantity { get; set; }
+        public string creditCard { get; set; }
+        public string expiration { get; set; }
+        public string securityCode { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string province { get; set; }
+        public string postalCode { get; set; }
+        public string country { get; set; }
+
+    }
+}
